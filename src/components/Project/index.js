259304e92f7299301a1e71ba96bb6dd08370d72c
Kelly Wacker <kelwacker@gmail.com>
2022-09-22 19:36:13 -0400

    project images

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,29 +6,29 @@ function Project() {
                 <div className="resume-section-content">
                     <h2 className="mb-5">Projects</h2>
                     <div className="mb-0 projects">
-                        <div className='project-card col-4'>
-                            <div className='project-img'><img href='!#' alt='' ></img></div>
-                            <h4>DnD Character Creator <a className="social-icon" href="https://github.com/tristristy/dnd-character-creator"><i className="fab fa-github"></i></a></h4>
+                        <div className='project-card col-5'>
+                            <div className='project-img'><img src={require(`../../assets/img/dnd.png`)} alt='' ></img></div>
+                            <h4><a href='https://dry-sands-22029.herokuapp.com/'>DnD Character Creator</a> <a className="social-icon" href="https://github.com/tristristy/dnd-character-creator"><i className="fab fa-github"></i></a></h4>
                         </div>
-                        <div className='project-card col-4'>
-                            <div className='project-img'><img href='!#' alt='' ></img></div>
-                            <h4>Note Tracker <a className="social-icon" href="https://github.com/kwax17/note-tracker"><i className="fab fa-github"></i></a></h4>
+                        <div className='project-card col-5'>
+                            <div className='project-img'><img src={require(`../../assets/img/note-taker.png`)} alt='' ></img></div>
+                            <h4><a href='https://nameless-wave-39366.herokuapp.com/'>Note Tracker</a> <a className="social-icon" href="https://github.com/kwax17/note-tracker"><i className="fab fa-github"></i></a></h4>
                         </div>
-                        <div className='project-card col-4'>
-                            <div className='project-img'><img href='!#' alt='' ></img></div>
-                            <h4>Weather App <a className="social-icon" href="https://github.com/kwax17/the-weather"><i className="fab fa-github"></i></a></h4>
+                        <div className='project-card col-5'>
+                            <div className='project-img'><img src={require(`../../assets/img/weather.png`)} alt='' ></img></div>
+                            <h4><a href='https://kwax17.github.io/the-weather/'>Weather App </a><a className="social-icon" href="https://github.com/kwax17/the-weather"><i className="fab fa-github"></i></a></h4>
                         </div>
-                        <div className='project-card col-4'>
-                            <div className='project-img'><img href='!#' alt='' ></img></div>
-                            <h4>Tech Blog <a className="social-icon" href="https://github.com/kwax17/the-social-network"><i className="fab fa-github"></i></a></h4>
+                        <div className='project-card col-5'>
+                            <div className='project-img'><img src={require(`../../assets/img/tech-blog.png`)} alt='' ></img></div>
+                            <h4><a href='https://fast-savannah-20928.herokuapp.com/'>Tech Blog </a><a className="social-icon" href="https://github.com/kwax17/the-social-network"><i className="fab fa-github"></i></a></h4>
                         </div>
-                        <div className='project-card col-4'>
-                            <div className='project-img'><img href='!#' alt='' ></img></div>
-                            <h4>Quizzy <a className="social-icon" href="https://github.com/kwax17/quizzy"><i className="fab fa-github"></i></a></h4>
+                        <div className='project-card col-5'>
+                            <div className='project-img'><img src={require(`../../assets/img/quizzy.png`)} alt='' ></img></div>
+                            <h4><a href='https://kwax17.github.io/quizzy/'>Quizzy </a><a className="social-icon" href="https://github.com/kwax17/quizzy"><i className="fab fa-github"></i></a></h4>
                         </div>
-                        <div className='project-card col-4'>
-                            <div className='project-img'><img href='!#' alt='' ></img></div>
-                            <h4>Team Generator<a className="social-icon" href="https://github.com/kwax17/team-generator"><i className="fab fa-github"></i></a></h4>
+                        <div className='project-card col-5'>
+                            <div className='project-img'><img src={require(`../../assets/img/contact-card.png`)} alt='' ></img></div>
+                            <h4><a href='whttps://guarded-oasis-43574.herokuapp.com/'> Contact Card</a><a className="social-icon" href="https://github.com/kwax17/team-generator"><i className="fab fa-github"></i></a></h4>
                         </div>
                     </div>
                 </div>
