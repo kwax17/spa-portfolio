0658ec5211990424d2e5e292234153e053f8e4ef
Kelly Wacker <kelwacker@gmail.com>
2022-10-13 15:52:06 -0400

    new proj

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,66 +1,100 @@
-import React, { useState } from 'react';
-import { validateEmail } from '../../utils/helpers';
+import React from 'react';
+// import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  // const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
-  const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
+  // const [errorMessage, setErrorMessage] = useState('');
+  // const { name, email, message } = formState;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      setFormState({ [e.target.name]: e.target.value });
-      console.log('Form', formState);
-    }
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (!errorMessage) {
+  //     setFormState({ [e.target.name]: e.target.value });
+  //     console.log('Form', formState);
+  //   }
+  // };
 
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage('Your email is invalid.');
-      } else {
-        setErrorMessage('');
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
-    }
-  };
+  // const handleChange = (e) => {
+  //   if (e.target.name === 'email') {
+  //     const isValid = validateEmail(e.target.value);
+  //     if (!isValid) {
+  //       setErrorMessage('Your email is invalid.');
+  //     } else {
+  //       setErrorMessage('');
+  //     }
+  //   } else {
+  //     if (!e.target.value.length) {
+  //       setErrorMessage(`${e.target.name} is required.`);
+  //     } else {
+  //       setErrorMessage('');
+  //     }
+  //   }
+  // };
 
   return (
-    <section className="resume-section" id="contact">
-      <div className="contact-section-content">
-        <h2 className="mb-5" data-testid='h2tag'>Contact Me</h2>
-        <form id="contact-form" onSubmit={handleSubmit} method='post' action="mailto:kwax@kwacks.net" >
-        <div>
-          <label className='mb-0' htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+    // <section className="resume-section" id="contact">
+    //   <div className="contact-section-content">
+    //     <h2 className="mb-5" data-testid='h2tag'>Contact Me</h2>
+    //     <form id="contact-form" onSubmit={handleSubmit} method='post' action="mailto:kwax@kwacks.net" >
+    //     <div>
+    //       <label className='mb-0' htmlFor="name">Name:</label>
+    //       <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+    //     </div>
+    //     <div>
+    //       <label className='mb-0' htmlFor="email">Email address:</label>
+    //       <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+    //     </div>
+    //     <div>
+    //       {/* <label className='mb-0 message' htmlFor="message">Message:</label> */}
+    //       <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+    //     </div>
+    //     {errorMessage && (
+    //       <div>
+    //         <p className="error-text">{errorMessage}</p>
+    //       </div>
+    //     )}
+    //     <button data-testid="button" type="submit" value="Send">Submit</button>
+    //     <input type="hidden" name="recipient" value="kwax@kwacks.net" />
+    //     <input type="hidden" name="subject" value="Subject" />
+    //     {/* <input type="hidden" name="redirect"  value="https://www.example.com/thanks.html" /> */}
+    //     {/* <input type="hidden" name="missing_fields_redirect"  value="https://www.example.com/error.html" /> */}
+    //     <input type="hidden" name="required" value="name,email,message" />
+    //   </form>
+    //   </div>
+    // </section>
+    <section id="contact" class="contact resume-section">
+      <div className="container" data-aos="fade-up">
+        {/* section header */}
+        <div className="section-header">
+          <h2>Contact</h2>
         </div>
-        <div>
-          <label className='mb-0' htmlFor="email">Email address:</label>
-          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
-        </div>
-        <div>
-          {/* <label className='mb-0 message' htmlFor="message">Message:</label> */}
-          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
+        
+        <div className="row gx-lg-0 gy-4">
+          <div className="col-lg-8">
+            <form action="https://formspree.io/f/mqkjbkkw" method="POST" className='justify-content-left'>
+              <div className="row">
+                <div className="col-md-6 form-group">
+                  <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required></input>
+                </div>
+                <div className="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required></input>
+                </div>
+              </div>
+              <div className="form-group mt-3">
+                <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required></input>
+              </div>
+              <div className="form-group mt-3">
+                <textarea className="form-control" name="message" rows="7" placeholder="Message" required></textarea>
+              </div>
+              <div className="my-3">
+              </div>
+              <div className="text-center"><button className='submitbtn' type="submit">Send Message</button></div>
+            </form>
           </div>
-        )}
-        <button data-testid="button" type="submit" value="Send">Submit</button>
-        <input type="hidden" name="recipient" value="kwax@kwacks.net" />
-        <input type="hidden" name="subject" value="Subject" />
-        {/* <input type="hidden" name="redirect"  value="https://www.example.com/thanks.html" /> */}
-        {/* <input type="hidden" name="missing_fields_redirect"  value="https://www.example.com/error.html" /> */}
-        <input type="hidden" name="required" value="name,email,message" />
-      </form>
+
+        </div>
+
       </div>
     </section>
   );
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,13 +6,13 @@ function Project() {
                 <div className="resume-section-content">
                     <h2 className="mb-5">Projects</h2>
                     <div className="mb-0 projects">
-                        <div className='project-card col-5'>
-                            <div className='project-img'><img src={require(`../../assets/img/dnd.png`)} alt='' ></img></div>
-                            <h4><a href='https://dry-sands-22029.herokuapp.com/'>DnD Character Creator</a> <a className="social-icon" href="https://github.com/tristristy/dnd-character-creator"><i className="fab fa-github"></i></a></h4>
+                    <div className='project-card col-5'>
+                        <div className='project-img'><img src={require(`../../assets/img/movieswipe.png`)} alt='' ></img></div>
+                            <h4><a href='https://movie-swipe-application.herokuapp.com/'>Movie Swipe</a> <a className="social-icon" href="https://github.com/kwax17/note-tracker"><i className="fab fa-github"></i></a></h4>
                         </div>
                         <div className='project-card col-5'>
-                            <div className='project-img'><img src={require(`../../assets/img/note-taker.png`)} alt='' ></img></div>
-                            <h4><a href='https://nameless-wave-39366.herokuapp.com/'>Note Tracker</a> <a className="social-icon" href="https://github.com/kwax17/note-tracker"><i className="fab fa-github"></i></a></h4>
+                            <div className='project-img'><img src={require(`../../assets/img/dnd.png`)} alt='' ></img></div>
+                            <h4><a href='https://dry-sands-22029.herokuapp.com/'>DnD Character Creator</a> <a className="social-icon" href="https://github.com/rrvallar/Project-3"><i className="fab fa-github"></i></a></h4>
                         </div>
                         <div className='project-card col-5'>
                             <div className='project-img'><img src={require(`../../assets/img/weather.png`)} alt='' ></img></div>
@@ -27,8 +27,8 @@ function Project() {
                             <h4><a href='https://kwax17.github.io/quizzy/'>Quizzy </a><a className="social-icon" href="https://github.com/kwax17/quizzy"><i className="fab fa-github"></i></a></h4>
                         </div>
                         <div className='project-card col-5'>
-                            <div className='project-img'><img src={require(`../../assets/img/contact-card.png`)} alt='' ></img></div>
-                            <h4><a href='whttps://guarded-oasis-43574.herokuapp.com/'> Contact Card</a><a className="social-icon" href="https://github.com/kwax17/team-generator"><i className="fab fa-github"></i></a></h4>
+                            <div className='project-img'><img src={require(`../../assets/img/note-taker.png`)} alt='' ></img></div>
+                            <h4><a href='https://nameless-wave-39366.herokuapp.com/'>Note Tracker</a> <a className="social-icon" href="https://github.com/kwax17/note-tracker"><i className="fab fa-github"></i></a></h4>
                         </div>
                     </div>
                 </div>
