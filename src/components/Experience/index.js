0b06990ff09a175a10d75f72df90b98cd1e94d79
Kelly Wacker <kelwacker@gmail.com>
2024-04-02 17:53:02 -0400

    updates

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -19,7 +19,7 @@ function Experience() {
                     <div className="flex-grow-1">
                         <h3 className="mb-0">Graphic Designer</h3>
                         <div className="subheading mb-3">The Sandpaper</div>
-                        <p>Employ creative methods to create, edit, and rearrange advertisements for a weekly paper, ensuring both a happy customer and engaging layout.</p>
+                        <p>Employ creative methods to create, edit, and rearrange advertisements for a weekly paper, ensuring both a happy customer and engaging layout</p>
                     </div>
                     <div className="flex-shrink-0"><span className="text-primary">June 2022 - September 2022</span></div>
                 </div>
@@ -27,8 +27,8 @@ function Experience() {
                     <div className="flex-grow-1">
                         <h3 className="mb-0">Graphic Designer</h3>
                         <div className="subheading mb-3">Senior Publishing Company</div>
-                        <p>Consult and collaborate with independent communities and their editorial staffs to create monthly newsletters, flyers, and advertisements.</p>
-                        <p>Correctly format and design easy-to-follow layouts, as well as use artistic and creative abilities to make it an enjoyable experience.</p>
+                        <p>Consult and collaborate with independent communities and their editorial staffs to create monthly newsletters, flyers, and advertisements</p>
+                        <p>Correctly format and design easy-to-follow layouts, as well as use artistic and creative abilities to make it an enjoyable experience</p>
                     </div>
                     <div className="flex-shrink-0"><span className="text-primary">October 2021 - May 2022</span></div>
                 </div>
@@ -36,8 +36,8 @@ function Experience() {
                     <div className="flex-grow-1">
                         <h3 className="mb-0">Photographer/Videographer</h3>
                         <div className="subheading mb-3">Freelance</div>
-                        <p>Work alongisde local businesses to create alluring, true-to-life social media imagery to attract customers.</p>
-                        <p>Capture scenic views and transform everyday life into beautiful moments and memories.</p>
+                        <p>Work alongisde local businesses to create alluring, true-to-life social media imagery to attract customers</p>
+                        <p>Capture scenic views and transform everyday life into beautiful moments and memories</p>
                     </div>
                     <div className="flex-shrink-0"><span className="text-primary">May 2020 - Present</span></div>
                 </div>
--- src/components/Photos/index.js
+++ src/components/Photos/index.js
@@ -1,63 +1,73 @@
-import React from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import React from 'react';
+// // Import Swiper React components
+// import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
-import 'swiper/css';
+// // Import Swiper styles
+// import 'swiper/css';
 
-// import Swiper core and required modules
-import { Navigation, Scrollbar, A11y } from 'swiper';
+// // import Swiper core and required modules
+// import { Navigation, Scrollbar, A11y } from 'swiper';
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/scrollbar';
+// // Import Swiper styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/scrollbar';
 
 
-function Photos() {
-    return (
-        <section className="resume-section" id="photos">
-            <div className="resume-section-content">
-                <h2 className="mb-5">Photos</h2>
-                <div className="mb-0 photos" col-5>
-                    <Swiper
-                        modules={[Navigation, Scrollbar, A11y]}
-                        spaceBetween={20}
-                        slidesPerView={2}
-                        navigation
-                        scrollbar={{ draggable: true }}
-                    >
-                        <SwiperSlide><img src={require(`../../assets/img/photo/1.webp`)} alt="1" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/2.webp`)} alt="2" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/3.webp`)} alt="3" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/4.webp`)} alt="4" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/5.webp`)} alt="5" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/7.webp`)} alt="7" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/8.webp`)} alt="8" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/9.webp`)} alt="9" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/10.webp`)} alt="10" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/11.webp`)} alt="11" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/12.webp`)} alt="12" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/13.webp`)} alt="13" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/14.webp`)} alt="14" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/15.webp`)} alt="15" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/16.webp`)} alt="16" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/17.webp`)} alt="17" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/18.webp`)} alt="18" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/19.webp`)} alt="19" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/20.webp`)} alt="20" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/21.webp`)} alt="21" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/22.webp`)} alt="22" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/23.webp`)} alt="23" /></SwiperSlide>
-                        <SwiperSlide><img src={require(`../../assets/img/photo/24.webp`)} alt="24" /></SwiperSlide>
-                    </Swiper>
-                </div>
-            </div>
-        </section>
-    );
+// function Photos() {
+//     return (
+//         <section className="resume-section" id="photos">
+//             <div className="resume-section-content">
+//                 <h2 className="mb-5">Photos</h2>
+//                 <div className="mb-0 photos" col-5>
+//                     <Swiper
+//                         modules={[Navigation, Scrollbar, A11y]}
+//                         spaceBetween={20}
+//                         slidesPerView={2}
+//                         navigation
+//                         scrollbar={{ draggable: true }}
+//                     >
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/1.webp`)} alt="1" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/2.webp`)} alt="2" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/3.webp`)} alt="3" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/4.webp`)} alt="4" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/5.webp`)} alt="5" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/7.webp`)} alt="7" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/8.webp`)} alt="8" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/9.webp`)} alt="9" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/10.webp`)} alt="10" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/11.webp`)} alt="11" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/12.webp`)} alt="12" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/13.webp`)} alt="13" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/14.webp`)} alt="14" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/15.webp`)} alt="15" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/16.webp`)} alt="16" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/17.webp`)} alt="17" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/18.webp`)} alt="18" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/19.webp`)} alt="19" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/20.webp`)} alt="20" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/21.webp`)} alt="21" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/22.webp`)} alt="22" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/23.webp`)} alt="23" /></SwiperSlide>
+//                         <SwiperSlide><img src={require(`../../assets/img/photo/24.webp`)} alt="24" /></SwiperSlide>
+//                     </Swiper>
+//                 </div>
+//             </div>
+//         </section>
+//     );
+// }
+
+// export default Photos;
+
+
+
+import PhotoAlbum from "react-photo-album";
+
+const photos = [
+  { src: "/img/photo/1", width: 800, height: 600 },
+  { src: "/img/photo/2", width: 1600, height: 900 },
+];
+
+export default function Gallery() {
+  return <PhotoAlbum layout="rows" photos={photos} img src={require(`../../assets/img/photo/1.webp`)} alt="1" />;
 }
-
-export default Photos;
-
-
-
